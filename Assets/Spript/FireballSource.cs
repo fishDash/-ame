426222a1da56d0f42313682a748bf3bd77896841
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-14 20:07:12 +0300

    Create FireballSource.cs

---
+++ Assets/Spript/FireballSource.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballSource : MonoBehaviour
+{
+    public Transform targetPoint;
+    public Camera cameraLink;
+    public float targetInSkyDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = cameraLink.ViewportPointToRay(new Vector3(0.5f, 0.7f, 0));
+
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
+            targetPoint.position = hit.point;
+        }
+        else
+        {
+            targetPoint.position = ray.GetPoint(targetInSkyDistance);
+        }
+
+        transform.LookAt(targetPoint.position);
+    }
+}
