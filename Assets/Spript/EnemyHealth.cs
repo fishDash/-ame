d9f9ad7d8cea63b5238ad8d30c51821c7ad57f01
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-14 20:03:25 +0300

    Create EnemyHealth.cs

---
+++ Assets/Spript/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float value = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
