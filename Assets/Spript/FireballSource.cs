d39f79a795d5bd16fd94c696498b1cd576009724
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-14 22:39:40 +0300

    Update FireballSource.cs

--- Assets/Spript/FireballSource.cs
+++ Assets/Spript/FireballSource.cs
@@ -10,7 +10,8 @@ public class FireballSource : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
