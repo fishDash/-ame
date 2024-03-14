1ab8d16f4607c2960358af404a8218ee7ff790fc
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-14 22:39:44 +0300

    Create PlayerHealth.cs

---
+++ Assets/Spript/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float value = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
