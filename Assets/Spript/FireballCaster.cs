48d2855493eae15e2be3901e3a7cc1b1610c3b1e
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-14 20:06:54 +0300

    Create FireballCaster.cs

---
+++ Assets/Spript/FireballCaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public Fireball fireballPrefab;
+    public Transform fireballSourceTransform;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+       if (Input.GetMouseButtonDown(0))
+       {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+       }
+    }
+}
