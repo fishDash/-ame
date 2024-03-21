1bd0dc8b75a29c042d2ca244ab705d83674a8911
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-21 15:55:39 +0300

    Update Explosion.cs

--- Assets/Spript/Explosion.cs
+++ Assets/Spript/Explosion.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    public float maxSize = 5;
+    public float speed = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.localScale += Vector3.one * Time.deltaTime * speed;
+
+        if (transform.localScale.x > maxSize)
+        {
+            Destroy(gameObject);
+        }
     }
 }
