447f07f42d269e8ae91811bdcddbbeebfa886ec5
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-21 14:11:54 +0300

    Create EnemyAttackDamageEvent.cs

---
+++ Assets/Spript/EnemyAttackDamageEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackDamageEvent : MonoBehaviour
+{
+public EnemyAI enemyAI;
+	public void AttackDamageEvent()
+	{
+		enemyAI.AttackDamage();
+	}
+}
