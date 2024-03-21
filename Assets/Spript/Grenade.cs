453f4906058b59fa8b791764da2424c9ce33945e
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-21 15:39:32 +0300

    Update Grenade.cs

--- Assets/Spript/Grenade.cs
+++ Assets/Spript/Grenade.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
+    public float delay = 3;
+    public GameObject explosion;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,17 @@ public class Grenade : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Explosion", delay);
+    }
+
+    private void Explosion()
+    {
+        Destroy(gameObject);
+        Instantiate(explosion);
+        explosion.transform.position = transform.position;
     }
 }
