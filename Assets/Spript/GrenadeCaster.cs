30004abd731d16128ea26fe64888036c14ee205a
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-21 14:26:26 +0300

    Update GrenadeCaster.cs

--- Assets/Spript/GrenadeCaster.cs
+++ Assets/Spript/GrenadeCaster.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GrenadeCaster : MonoBehaviour
 {
+    public Rigidbody grenadePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,9 @@ public class GrenadeCaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(1))
+        {
+
+        }
     }
 }
