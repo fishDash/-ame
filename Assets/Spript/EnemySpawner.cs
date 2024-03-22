715994d01ee0f33ca50c831df7f1392bbfb5b69e
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-22 18:18:14 +0300

    Update EnemySpawner.cs

--- Assets/Spript/EnemySpawner.cs
+++ Assets/Spript/EnemySpawner.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public EnemyHealth _enemyHealth;
+    public EnemyAI enemyPrefab;
+    public PlayerController player;
+
+    public int enimesMaxCount = 5;
+    public float delay = 3;
+    public float increaseEnemiesCountDelay = 30;
+
+    public List<Transform> _spawnerPoint;
+    public List<Transform> patrolPoints;
+    public List<EnemyAI> _enemies;
+
+    private float _timeLastSpawned;
+
+    private void Start()
     {
-        
+        _spawnerPoint = new List<Transform>(transform.GetComponentsInChildren<Transform>());
+        _enemies = new List<EnemyAI>();
+
+        Invoke("IncreaseEnemies", increaseEnemiesCountDelay);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void IncreaseEnemies()
     {
-        
+        enimesMaxCount++;
+        Invoke("IncreaseEnemies", increaseEnemiesCountDelay);
+    }
+
+    private void IsAlive()
+    {
+        _enemyHealth.value > 0 return;
+    }
+
+    private void Update()
+    {
+        CheekForDeadEnenmies();
+
+        CreateEnemy();
+    }
+
+    private void CheekForDeadEnenmies()
+    {
+        for(var i = 0; i < _enemies.Count; i ++)
+        {
+            if (_enemies[i].IsAlive())
+            {
+                continue;
+            }
+            _enemies.RemoveAt(i);
+            i--;
+        }
+    }
+
+    private void CreateEnemy()
+    {
+        if (_enemies.Count >= enimesMaxCount)
+        {
+            return;
+        }
+
+        if (Time.time - _timeLastSpawned < delay)
+        {
+            return;
+        }
+
+        var enemy = Instantiate(enemyPrefab);
+        enemy.transform.position = _spawnerPoint[Random.Range(0, _spawnerPoint.Count)].position;
+        enemy.player = player;
+        enemy.patrolPoints = patrolPoints;
+        _enemies.Add(enemy);
+        _timeLastSpawned = Time.time;
     }
 }
