74ac101411ad58023e3313d040075ef9e8069bce
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-23 14:52:24 +0300

    Update Vulcano.cs

--- Assets/Spript/Vulcano.cs
+++ Assets/Spript/Vulcano.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Vulcano : MonoBehaviour
 {
    public Grenade grenadePrefab;
+   public float force = 500;
 
    private void SpawnGrenade()
    {
-
+	   var grenade = Instantiate(grenadePrefab);
+       grenade.transform.position = transform.position;
+       grenade.GetComponent<Rigidbody>().AddForce(transform.forward * force);
    }
 }
