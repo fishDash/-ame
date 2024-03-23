552334019473fe3179cdc080e3f9805dd0ff7426
⋆˙⟡♡‧₊˚ ☁️⋅♡ <100473772+fishDash@users.noreply.github.com>
2024-03-23 15:17:11 +0300

    Update Vulcano.cs

--- Assets/Spript/Vulcano.cs
+++ Assets/Spript/Vulcano.cs
@@ -6,11 +6,19 @@ public class Vulcano : MonoBehaviour
 {
    public Grenade grenadePrefab;
    public float force = 500;
+   public float delayMin = 1;
+   public float delayMax = 3;
+
+   private void Start()
+   {
+       Invoke("SpawnGrenade", Random.Range(delayMin, delayMax));
+   }
 
    private void SpawnGrenade()
    {
 	   var grenade = Instantiate(grenadePrefab);
        grenade.transform.position = transform.position;
        grenade.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+       Invoke("SpawnGrenade", Random.Range(delayMin, delayMax));
    }
 }
